c78ac79dae8503f03a6198bdc86386badbdb626c
chen08209 <chen08209@gmail.com>
2026-01-19 16:50:16 +0800

    cache

--- linux/runner/my_application.cc
+++ linux/runner/my_application.cc
@@ -63,7 +63,7 @@ static void my_application_activate(GApplication* application) {
   // Background defaults to black, override it here if necessary, e.g. #00000000 for transparent.
   gdk_rgba_parse(&background_color, "#000000");
   fl_view_set_background_color(view, &background_color);
-  gtk_widget_realize(GTK_WIDGET(window));
+  gtk_widget_show(GTK_WIDGET(view));
   gtk_container_add(GTK_CONTAINER(window), GTK_WIDGET(view));
 
   // Show the window when Flutter renders.
